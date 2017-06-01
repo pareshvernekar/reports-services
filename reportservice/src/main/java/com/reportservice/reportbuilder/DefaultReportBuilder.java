8da4c6e6114785c4788f95af451eb89d4bb3e265
Paresh Vernekar <fremontguy14@gmail.com>
2017-05-31 22:10:44 -0700

    Initial service class

---
+++ reportservice/src/main/java/com/reportservice/reportbuilder/DefaultReportBuilder.java
@@ -0,0 +1,33 @@
+package com.reportservice.reportbuilder;
+
+import java.util.List;
+
+import com.reportservice.domain.ReportColumn;
+
+public class DefaultReportBuilder extends ReportBuilder {
+
+	@Override
+	public void instantiate() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setReportColumns(List<ReportColumn> reportColumns) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setTitle(String title) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setSubTitle(String subtitle) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ reportservice/src/main/java/com/reportservice/service/RepoortBuilderService.java
@@ -0,0 +1,15 @@
+package com.reportservice.service;
+
+import javax.transaction.Transactional;
+
+/*
+ * Facade to build a report
+ */
+@Transactional
+public class RepoortBuilderService {
+
+	
+	public void generateReport() {	//Need to add parameters
+		
+	}
+}
